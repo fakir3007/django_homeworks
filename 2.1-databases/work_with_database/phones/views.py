b8d4abfc69ceb58d3efc359a374f925b82e210f8
fakir3007 <91556351+fakir3007@users.noreply.github.com>
2022-09-04 09:57:35 +0500

    change_views

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render, redirect
 
 
-def index(request):
-    return redirect('catalog')
-
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
-    return render(request, template, context)
+    sort_pages = request.GET.get('sort')
+    phones_objects = Phone.objects.all()
+
+    if sort_pages == 'max_price':
+        phones_objects = phones_objects.order_by('price').reverse()
+    elif sort_pages == 'min_price':
+        phones_objects = phones_objects.order_by('price')
+    elif sort_pages == 'name':
+        phones_objects = phones_objects.order_by('name')
+
+    context = {'phones': phones_objects,
+               }
+    return render(request, template, context=context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
-    return render(request, template, context)
+    phone = Phone.objects.get(slug=slug)
+    context = {'phone': phone}
+    return render(request, template, context=context)
